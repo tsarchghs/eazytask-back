54e4b16119e19ee31c68b70d622733a56b6f1c1b
Gjergj Kadriu <gjergjk71@gmail.com>
2020-04-23 20:19:38 +0200

    Created Task model

---
+++ models/Task.js
@@ -0,0 +1,29 @@
+"use strict"
+
+const { NonNullString, NonNullStatusField } = require("./common")
+
+module.exports = (sequelize, DataTypes) => {
+    let Task = sequelize.define("Task", {
+        thumbnail: NonNullString(DataTypes.STRING),
+        title: NonNullString(DataTypes.STRING),
+        description: NonNullString(DataTypes.STRING),
+        due_date_type: {
+            type: DataTypes.ENUM("FIXED_DATE","UNTIL_DATE"),
+            allowNull: false
+        },
+        due_date: {
+            type: DataTypes.DATE,
+            allowNull: false
+        },
+        location: NonNullString(DataTypes.STRING),
+        expected_price: {
+            type: DataTypes.FLOAT,
+            allowNull: false
+        },
+        status: NonNullStatusField(DataTypes.ENUM)
+    })
+    Task.associate = models => {
+        Task.belongsTo(models.User)
+    }
+    return Task;
+}
