e518206ec4077e630c318867cf9f6a6dc2bbd308
Gjergj Kadriu <gjergjk71@gmail.com>
2020-04-23 20:18:38 +0200

    Created Skill model

---
+++ models/Skill.js
@@ -0,0 +1,20 @@
+"use strict"
+
+const { NonNullUniqueString } = require("./common")
+
+module.exports = (sequelize, DataTypes) => {
+    let Skill = sequelize.define("Skill",{
+        name: NonNullUniqueString(DataTypes.STRING)
+    })
+    Skill.associate = models => {
+        console.log(models.TaskerSkill)
+   Skill.belongsToMany(
+            models.Tasker, 
+            { 
+                through: models.Tasker_Skill, 
+                foreignKey: "SkillId"
+            }
+        );
+    }
+    return Skill;
+}
