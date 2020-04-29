d7948bbb16a90a7bb672da5b2a7b9760bf8791d2
Gjergj Kadriu <gjergjk71@gmail.com>
2020-04-29 23:25:42 +0200

    added bodyParser

--- app.js
+++ app.js
@@ -2,11 +2,17 @@ require('dotenv').config()
 
 const express = require('express')
 const compression = require("compression");
+const models = require("./models");
+const bodyParser = require("body-parser")
 
 const app = express()
 const PORT = process.env.PORT || 4000
 
+// models.sequelize.drop() 
+models.sequelize.sync({ force: true });
 app.use(compression())
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.json())
 
 app.get('/', (req, res) => res.send('Test'))
 
