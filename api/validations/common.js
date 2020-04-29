7e98dde154349b872b92a24e242c97bbecf0eee0
Gjergj Kadriu <gjergjk71@gmail.com>
2020-04-29 23:58:56 +0200

    Added common fields

--- api/validations/common.js
+++ api/validations/common.js
@@ -1,5 +1,7 @@
 const yup = require("yup");
 
 module.exports = {
+    id: yup.number().positive().integer(),
     password: yup.string().required().min(6),
+    user_offers_fields: yup.array().of(yup.string().oneOf(["user", "offers"])),
 }
