f53509b11a5b738e882ad174e31a5424f2e6903b
Gjergj Kadriu <gjergjk71@gmail.com>
2020-05-01 00:14:43 +0200

    Created run function

--- app.js
+++ app.js
@@ -1,32 +1,44 @@
 require('dotenv').config()
-
 const express = require('express')
 const compression = require("compression");
-const models = require("./models");
 const bodyParser = require("body-parser")
-
-const app = express()
-const PORT = process.env.PORT || 4000
-const MainRouter = require("./api");
-
 const swaggerUi = require('swagger-ui-express');
 const swaggerDocument = require('./swagger.json');
+const logger = require("morgan")("dev")
 
-app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+const run = async ({ app, port, resetDb, database}) => {
+    let models = require("./models")[database];
+    console.log(999,{models})
+    const PORT = port || process.env.PORT || 4000
+    const MainRouter = require("./api");
 
-// models.sequelize.drop() 
-models.sequelize.sync({ force: true });
-app.use(compression())
-app.use(bodyParser.urlencoded({ extended: true }))
-app.use(bodyParser.json())
+    app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
-app.use((req, res, next) => {
-    if (req.body.email) req.body.email = req.body.email.toLowerCase()
-    next();
+    if (resetDb){
+        models.sequelize.drop() 
+    }
+    // console.log({ models.seq.sync})
+    models.sequelize.sync({ force: true });
+    app.use(compression())
+    app.use(bodyParser.urlencoded({ extended: true }))
+    app.use(bodyParser.json())
+    app.use(logger)
+
+    app.use((req, res, next) => {
+        if (req.body.email) req.body.email = req.body.email.toLowerCase()
+        next();
+    })
+    
+    app.use("/api/v1",MainRouter({database}))
+    app.get('/', (req, res) => res.send('Test'))
+    
+    app.listen(PORT, () => console.log(`Listening at http://localhost:${PORT}`))
+}
+
+
+if (require.main === module) run({ 
+    app: express(), 
+    database: "eazytask", 
 })
 
-app.use("/api/v1",MainRouter)
-
-app.get('/', (req, res) => res.send('Test'))
-
-app.listen(PORT, () => console.log(`Listening at http://localhost:${PORT}`))
+module.exports = run
