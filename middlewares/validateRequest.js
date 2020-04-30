150a6794af24e6cdd279c5e3a8eb326c0377ad32
Gjergj Kadriu <gjergjk71@gmail.com>
2020-05-01 00:17:09 +0200

    Weird fix

--- middlewares/validateRequest.js
+++ middlewares/validateRequest.js
@@ -3,10 +3,14 @@
 module.exports = yupSchema => {
     return async (req,res,next) => {
         yupSchema.validate({
-            requestBody: req.body,
+            requestBody: req.body.body ? req.body.body : req.body,
             query: req.query
         },{abortEarly: false}).catch(err => {
-            console.log({err})
+            console.log({res:{
+                message: "Validation error",
+                code: 403,
+                errors: err.errors
+            }, withBody: req.body.body ? req.body.body : req.body})
             return res.send({
                 message: "Validation error",
                 code: 403,
