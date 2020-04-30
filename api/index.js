567716642866df42bed886469bf4891d13d8ff5c
Gjergj Kadriu <gjergjk71@gmail.com>
2020-05-01 00:14:57 +0200

    Passes database variable

--- api/index.js
+++ api/index.js
@@ -1,7 +1,10 @@
 const MainRouter = require("express").Router();
 
-// MainRouter.use("/auth", require("./auth"))
-MainRouter.get("/",(req,res) => res.send("API v1"))
-MainRouter.use("/users", require("./users"))
 
-module.exports = MainRouter
+// MainRouter.use("/auth", require("./auth"))
+
+module.exports = ({database}) => {
+    MainRouter.get("/",(req,res) => res.send("API v1"))
+    MainRouter.use("/users", require("./users")({database}))
+    return MainRouter
+}
