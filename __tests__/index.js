3dde79c270b6ec741b26ce04197efea9dfa8aaa3
Gjergj Kadriu <gjergjk71@gmail.com>
2020-05-01 01:27:21 +0200

    Starts server before testing

--- __tests__/index.js
+++ __tests__/index.js
@@ -1,16 +1,33 @@
 const fetch = require('node-fetch');
 const users_cases = require("./users");
 
-jest.setTimeout(10000);
+const { setup: setupDevServer } = require('jest-dev-server')
+const { teardown: teardownDevServer } = require('jest-dev-server')
+
+jest.setTimeout(25000);
 
 let cases = [...users_cases]
 
 let port = 5124  
 let URI = () => `http://localhost:${port}/api/v1`
 
+beforeAll(async () => {
+    setupDevServer({
+        command: `node ./testServer.js`,
+        launchTimeout: 50000,
+        port: 3000,
+    })
+    await new Promise(resolve => setTimeout(resolve, 5000));
+})
+
+afterAll(async done => {
+    teardownDevServer()
+    done()
+})
+
 cases.forEach(case_ => {
     describe(case_.title, () => {   
-        it(case_.description, async () => {
+        it(case_.description, async done => {
             if (case_.before) await case_.before()
             let api = `${URI()}${case_.path}`
             case_.request.body = JSON.stringify(case_.request.body)
@@ -22,7 +39,8 @@ cases.forEach(case_ => {
                 delete data.data.password
             }
             expect(JSON.stringify(data))
-                .toBe(JSON.stringify(case_.response))        
+                .toBe(JSON.stringify(case_.response))
+            done()     
         })
     })
 
