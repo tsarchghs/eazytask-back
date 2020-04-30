8e7d3714e8ec9802a9063a243663fd5baceedb05
Gjergj Kadriu <gjergjk71@gmail.com>
2020-04-30 14:21:31 +0200

    Used MainRouter, added swagger docs

--- app.js
+++ app.js
@@ -7,12 +7,25 @@ const bodyParser = require("body-parser")
 
 const app = express()
 const PORT = process.env.PORT || 4000
+const MainRouter = require("./api");
+
+const swaggerUi = require('swagger-ui-express');
+const swaggerDocument = require('./swagger.json');
+
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 // models.sequelize.drop() 
 models.sequelize.sync({ force: true });
 app.use(compression())
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json())
+
+app.use((req, res, next) => {
+    if (req.body.email) req.body.email = req.body.email.toLowerCase()
+    next();
+})
+
+app.use("/api/v1",MainRouter)
 
 app.get('/', (req, res) => res.send('Test'))
 
