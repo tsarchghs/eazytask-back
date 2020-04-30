9af8acf300d1d7ba9e0a62ee403876a7c6b5c68e
Gjergj Kadriu <gjergjk71@gmail.com>
2020-04-30 13:03:18 +0200

    Minor fix

--- api/validations/users/post.js
+++ api/validations/users/post.js
@@ -2,7 +2,7 @@ const yup = require("yup");
 const { password } = require("../common");
 
 module.exports = yup.object().shape({
-    query: yup.object().shape({
+    requestBody: yup.object().shape({
         first_name: yup.string().required().min(2),
         lastname: yup.string().required().min(2),
         email: yup.string().required().email(),
