a291fa78864f587037046f8b18a3e8da3fedd9ae
Gjergj Kadriu <gjergjk71@gmail.com>
2020-05-01 15:22:47 +0200

    Implemented post /auth and minor change in post /users

--- api/users.js
+++ api/users.js
@@ -1,19 +1,17 @@
 const validateRequest = require("../middlewares/validateRequest");
 const post_users = require("./validations/users/post")
 const bcrypt = require("bcrypt")
+const { SALT_ROUNDS } = require("../configs");
 const router = require("express").Router();
-
-const saltRounds = 10;
 
 
 module.exports = ({ database}) => {
-  console.log(1111,database)
   let { User } = require("../models")[database]
 
   router.post('/', validateRequest(post_users), async (req, res) => {
     console.log({ User }, 9991)
     console.log({body:req.body})
-    bcrypt.hash(req.body.password, saltRounds, async (err, hashedPassword) => {
+    bcrypt.hash(req.body.password, SALT_ROUNDS, async (err, hashedPassword) => {
       if (err) throw err
       if (!req.body.notification_option) req.body.notification_option = "EMAIL"
       let user;
@@ -23,6 +21,7 @@ module.exports = ({ database}) => {
       } catch (err) {
           console.log(123,err)
           return res.send({
+            status: "error",
             code: 409,
             message: "Cannot create resource because it conflicts with the current state of the server.",
             errors: err.errors.map(err => {
