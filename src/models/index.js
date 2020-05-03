aa0c3c1faca7603322faf74284f775b1d4617b48
Gjergj Kadriu <gjergjk71@gmail.com>
2020-05-03 21:26:05 +0200

    Testing log

--- src/models/index.js
+++ src/models/index.js
@@ -10,6 +10,7 @@ var basename = path.basename(__filename);
 
 var db = {};
 
+console.log(process.env.MYSQL_DATABASE, process.env.MYSQL_USERNAME, process.env.MYSQL_PASSWORD,process.env.MYSQL_HOST)
 const sequelize = new Sequelize(process.env.MYSQL_DATABASE, process.env.MYSQL_USERNAME, process.env.MYSQL_PASSWORD, {
     host: process.env.MYSQL_HOST,
     dialect: "mysql"/* one of 'mysql' | 'mariadb' | 'postgres' | 'mssql' */,
