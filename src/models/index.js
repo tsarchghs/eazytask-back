0083b037aa5d1a2764819425ae370ce4c5d89992
Gjergj Kadriu <gjergjk71@gmail.com>
2020-05-03 21:31:25 +0200

    Testing log

--- src/models/index.js
+++ src/models/index.js
@@ -16,6 +16,7 @@ if (process.env.IN_TRAVIS){
     process.env.MYSQL_PASSWORD = undefined
     process.env.MYSQL_HOST = "localhost"
 }
+console.log(process.env.IN_TRAVIS,919)
 const sequelize = new Sequelize(process.env.MYSQL_DATABASE, process.env.MYSQL_USERNAME, process.env.MYSQL_PASSWORD, {
     host: process.env.MYSQL_HOST,
     dialect: "mysql"/* one of 'mysql' | 'mariadb' | 'postgres' | 'mssql' */,
