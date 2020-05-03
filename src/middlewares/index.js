afd1bb9624980b0b8a9a61eff7a363ec2547a4fa
Gjergj Kadriu <gjergjk71@gmail.com>
2020-05-03 21:05:02 +0200

    Exported middlewares

---
+++ src/middlewares/index.js
@@ -0,0 +1,9 @@
+const errorHandler = require("./errorHandler");
+const validateRequest = require("./validateRequest")
+const caseInsensitiveEmail = require("./caseInsensitiveEmail")
+
+module.exports = {
+    errorHandler,
+    validateRequest,
+    caseInsensitiveEmail
+}
