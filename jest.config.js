e438a62e7991c724bd28947827e3b6ea9682745f
Gjergj Kadriu <gjergjk71@gmail.com>
2020-05-03 21:05:50 +0200

    Jest configurations/Packages updated

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,17 @@
+
+// require('dotenv').config({ path: "__test__/.test.env" })
+
+process.env.MYSQL_HOST = "localhost"
+process.env.MYSQL_DATABASE = "eazytask_test"
+process.env.MYSQL_USERNAME = "root"
+process.env.MYSQL_PASSWORD = "gjergji.123"
+process.env.DISABLE_SQL_LOGGING = true
+process.env.DROP_TABLES = true
+process.env.FORCE_SYNC_DB = true
+process.env.CUSTOM_ENV_PATH = true
+
 module.exports = {
     setupFilesAfterEnv: ['./jest.setup.js'],
     collectCoverage: true,
-    collectCoverageFrom:["api/*","loaders/*","middlewares/*","models/*","*.js"]
+    collectCoverageFrom: ["src/*.js", "src/*/*.js", "src/*/*/*.js"]
 };
