e9694e1ff6bc516ddfe99984d16544c7dc8f0732
Gjergj Kadriu <gjergjk71@gmail.com>
2020-05-04 13:08:14 +0200

    Implemented GET /auth

--- src/lib/auth-api/index.js
+++ src/lib/auth-api/index.js
@@ -1,23 +1,32 @@
 const express = require("express");
 const app = module.exports = express();
 
-const validateRequest = require("../../middlewares/validateRequest");
+const { validateRequest, jwtRequired } = require("../../middlewares");
 const { post_auth } = require("./validations")
 const createToken = require("./createToken");
 const validateCredentials = require("./validateCredentials")
 
+const { findUserByPk } = require("../users-dal");
+
+const getResponse = user => ({
+    status: "success",
+    code: 200,
+    message: "Authorized",
+    data: {
+        token: createToken(user.id),
+        user,
+        expires_in: 10000
+    }
+})
+
+app.get('/auth', jwtRequired, async (req, res) => {
+    let user = await findUserByPk(req.auth.userId)
+    return res.json(getResponse(user))
+});
+
 app.post('/auth', validateRequest(post_auth), async (req, res) => {
     let user = await validateCredentials(req.body)
-    return res.send({
-        status: "success",
-        code: 200,
-        message: "Authorized",
-        data: {
-            token: createToken(user.id),
-            user,
-            expires_in: 10000
-        }
-    })
+    return res.json(getResponse(user))
 });
 
 module.exports = app;
