5c8ac4e530d302d9e2fc4da02b649876ed1778fb
Gjergj Kadriu <gjergjk71@gmail.com>
2020-05-11 01:21:03 +0200

    Created categories api

---
+++ src/lib/categories-api/index.js
@@ -0,0 +1,33 @@
+
+const express = require("express");
+const app = module.exports = express();
+
+const { validateRequest, jwtRequired, passUserFromJWT, requireAdminAccess } = require("../../middlewares");
+const { post_categories } = require("./validations");
+
+const { findAll, createCategory } = require("./categories-dal");
+
+
+app.get("/categories", async (req, res) => {
+    let categories = await findAll()
+    return res.json({
+        message: "success",
+        status: 200,
+        data: categories
+    })
+})
+
+
+app.post("/categories",[
+    validateRequest(post_categories),
+    jwtRequired,
+    passUserFromJWT,
+    requireAdminAccess
+], async (req, res) => {
+    let category = await createCategory({name: req.body.name})
+    return res.json({
+        message: "success",
+        status: 200,
+        data: category
+    })
+})
