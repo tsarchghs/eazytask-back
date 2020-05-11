c9644dbd8065bed210bb96b636096467be6fbef3
Gjergj Kadriu <gjergjk71@gmail.com>
2020-05-11 12:32:43 +0200

    Confirmed/Changing

--- src/app.js
+++ src/app.js
@@ -25,7 +25,7 @@ const taskers_api = require("./lib/taskers-api");
 const app = express();
 
 const allowCrossDomain = function(req, res, next) {
-    res.header('Access-Control-Allow-Origin', 'example.com');
+    res.header('Access-Control-Allow-Origin', '*');
     res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
     res.header('Access-Control-Allow-Headers', 'Content-Type');
 
