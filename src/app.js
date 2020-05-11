095ef73e708d0afa2ec974db22c0b114fd09bbaa
Gjergj Kadriu <gjergjk71@gmail.com>
2020-05-11 12:48:04 +0200

    cors configs

--- src/app.js
+++ src/app.js
@@ -27,7 +27,7 @@ const app = express();
 const allowCrossDomain = function(req, res, next) {
     res.header('Access-Control-Allow-Origin', '*');
     res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
-    res.header('Access-Control-Allow-Headers', 'Content-Type');
+    res.header("Access-Control-Allow-Headers", "Origin, Accept, Authorization, Content-Type, Access-Control-Allow-Headers, X-Requested-With");
 
     next();
 }
