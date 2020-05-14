5a27f0f32b6a4dae4fdc6ea48d301f6b6c83d2e9
Gjergj Kadriu <gjergjk71@gmail.com>
2020-05-14 13:06:07 +0200

    Cors Configuration

--- src/middlewares/allowCrossDomain.js
+++ src/middlewares/allowCrossDomain.js
@@ -1,15 +1,18 @@
+const cors = require('cors')
 
-module.exports = function (req, res, next) {
-    console.log("SETTING HEADERS")
-    res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
-    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
+module.exports = cors()
 
-    // intercept OPTIONS method
-    if ('OPTIONS' == req.method) {
-        res.send(200);
-    }
-    else {
-        next();
-    }
-};
+// module.exports = function (req, res, next) {
+//     console.log("SETTING HEADERS")
+//     res.header('Access-Control-Allow-Origin', '*');
+//     res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+//     res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
+
+//     // intercept OPTIONS method
+//     if ('OPTIONS' == req.method) {
+//         res.send(200);
+//     }
+//     else {
+//         next();
+//     }
+// };
