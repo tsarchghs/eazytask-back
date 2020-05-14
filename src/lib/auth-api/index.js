c632310eb7a807d9d8ba2a66a604a02e827501ca
Gjergj Kadriu <gjergjk71@gmail.com>
2020-05-14 12:39:53 +0200

    TESTING LOG

--- src/lib/auth-api/index.js
+++ src/lib/auth-api/index.js
@@ -21,6 +21,7 @@ const getResponse = user => ({
 
 app.get('/auth', jwtRequired, async (req, res) => {
     let user = await findUserByPk(req.auth.userId)
+    console.log({user})
     if (!user) throw new ErrorHandler(401, "Unauthorized")
     return res.json(getResponse(user))
 });
--- src/middlewares/allowCrossDomain.js
+++ src/middlewares/allowCrossDomain.js
@@ -1,5 +1,6 @@
 
 module.exports = function (req, res, next) {
+    console.log("SETTING HEADERS")
     res.header('Access-Control-Allow-Origin', '*');
     res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
     res.header("Access-Control-Allow-Headers", "Origin, Accept, Authorization, Content-Type, Access-Control-Allow-Headers, X-Requested-With");
