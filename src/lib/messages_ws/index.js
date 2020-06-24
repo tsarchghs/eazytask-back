6bd7c401c08141fb354a22c2883cba62937faf0e
Gjergj Kadriu <gjergjk71@gmail.com>
2020-06-24 18:48:50 +0200

    Created the real-time part for QA(Chat) feature

---
+++ src/lib/messages_ws/index.js
@@ -0,0 +1,21 @@
+
+module.exports = server => {
+    const io = require("socket.io")(server);
+    console.log(123)
+    io.on("connection", socket => {
+        console.log("ON_CONNECTION")
+        
+        socket.on("send_message", msg => {
+            io.to('task/' + msg.taskId).emit("share_message", msg);
+        })
+        
+        socket.on("join_room", data => {
+            socket.join(data.room_name)
+            socket.to('some room')
+        });
+
+        socket.on("disconnect", () => {
+            console.log("ON_DISCONNECTION")
+        })
+    })
+}
