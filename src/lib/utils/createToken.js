2fc653ef7bc03772b3047737ae7b39fce35c1051
Gjergj Kadriu <gjergjk71@gmail.com>
2020-06-26 16:17:12 +0200

    Modified createToken function

--- src/lib/utils/createToken.js
+++ src/lib/utils/createToken.js
@@ -1,9 +1,12 @@
 const jwt = require("jsonwebtoken")
 const { JWT_SECRET } = require("../../configs")
 
-module.exports = (userId) => {
+module.exports = (userId, extra = {}, expiresIn) => {
+    let options = {}
+    if (expiresIn) options.expiresIn = expiresIn;
     const token = jwt.sign({
-        userId: userId
-    }, JWT_SECRET);
+        userId: userId,
+        ...extra
+    }, JWT_SECRET, options);
     return token
 }
