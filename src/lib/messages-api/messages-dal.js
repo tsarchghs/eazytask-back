94f5acf4acd413184b65b9126c140120467a10e2
Gjergj Kadriu <gjergjk71@gmail.com>
2020-06-29 16:26:17 +0200

    Include User relation to message related DAL functions

--- src/lib/messages-api/messages-dal.js
+++ src/lib/messages-api/messages-dal.js
@@ -1,17 +1,17 @@
 
-const { Message } = require("../../models")
-
+const { Message, User } = require("../../models")
 
 module.exports = {
     findAll: async ({taskId}) => {
         let where = {}
         if (taskId) where["TaskId"] = Number(taskId);
-        return Message.findAll({where});
+        return Message.findAll({where, include: [ User ]});
     },
     createMessage: async (data,currentUserId) => {
         data.client_createdAt = new Date(data.client_createdAt);
         data.UserId = currentUserId;
-        return Message.create(data)
+        let message = Message.create(data);
+        return Message.findOne({ where: { id: message.id }});
     }
         
 }
