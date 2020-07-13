ab3ee27b021d273fd2382a3e01374653d942ff4e
Gjergj Kadriu <gjergjk71@gmail.com>
2020-07-13 02:05:37 +0200

    Created phone-manager lib

---
+++ src/lib/phone-manager/index.js
@@ -0,0 +1,28 @@
+const client = require('twilio')(
+    process.env.TWILIO_ACCOUNT_SID, 
+    process.env.TWILIO_AUTH_TOKEN
+);
+
+const getPhoneVerificationCodeSMS = ({ code, to }) => {
+    return {
+        from: process.env.TWILIO_NUMBER, to,
+        body: "Verification code: " + code
+    }
+}
+
+const sendSMS = async ({ body, from, to }) => {
+    return await client.messages.create({
+        body, from, to
+    })
+}
+
+const sendPhoneVerificationCodeSMS =  async ({ code, to }) => {
+    let args = getPhoneVerificationCodeSMS({ code, to })
+    await sendSMS(args);
+}
+
+module.exports = {
+    getPhoneVerificationCodeSMS,
+    sendPhoneVerificationCodeSMS,
+    sendSMS
+}
