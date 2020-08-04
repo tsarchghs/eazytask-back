a939decf827f4972e04f5139d90a0b8f7247a649
Gjergj Kadriu <gjergjk71@gmail.com>
2020-08-04 06:35:54 +0200

    uuid

--- src/lib/users-api/users-dal.js
+++ src/lib/users-api/users-dal.js
@@ -14,7 +14,7 @@ const cloneDeep = require("../utils/cloneDeep");
 const { createTasker } = require("../taskers-api/taskers-dal");
 const emailManager = require("../email-manager");
 
-const uuid = require("uuid");
+const uuidv1 = require("uuid").v1;
 
 const FIELD_MODEL = {
     tasker: {
@@ -84,7 +84,7 @@ module.exports = {
         console.log(patchFields.isTasker, typeof(patchFields.isTasker),"patchFields.isTasker")
         let afterUpdate;
         if (patchFields.deleted) {
-            patchFields.email = user.email + "-" + uuid.v1()
+            patchFields.email = user.email + "-" + uuidv1()
         }
         if (patchFields.isTasker !== undefined) {
             let send = async (id, isTasker, setupCompleted) => {
