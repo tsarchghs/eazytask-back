f15bef3b33f3011a7d24956a77f22af3dde71139
Gjergj Kadriu <gjergjk71@gmail.com>
2020-09-02 11:05:24 +0200

    SMS translated

--- src/lib/notifications-manager/sms-notifications.js
+++ src/lib/notifications-manager/sms-notifications.js
@@ -1,16 +1,16 @@
 
 module.exports = {
     "OFFER_RECEIVED": ({ user_1, user_2, task }) => ({
-        body: `${user_2.first_name} ${user_2.last_name[0]} made an offer at “${task.title}”\nTask: ${process.env.BASE_URL}/task/${task.id}`,
+        body: `${user_2.first_name} ${user_2.last_name[0]} hat ein Angebot für “${task.title}” \nTask: ${process.env.BASE_URL}/task/${task.id}`,
         to: user_1.phone_number
     }),
     "OFFER_ACCEPTED": ({ user_1, user_2, task }) => ({
-        body: `${user_2.first_name} ${user_2.last_name[0]} accepted your offer for “${task.title}”`,
+        body: `${user_2.first_name} ${user_2.last_name[0]} hat dein Angebot für “${task.title}”`,
         to: user_1.phone_number
     }),
     "AFTER_OFFER_ACCEPTED": ({ user_1, user_2, task }) => {
-        let body = `Contact Information for ${user_2.first_name} ${user_2.last_name[0]}. :\n`
-        body += `${user_2.phone_number == "+41" ? "" : `Number: ${user_2.phone_number}\n`}Email: ${user_2.email}\nFull Name: ${user_2.first_name} ${user_2.last_name}\n
+        let body = `Gerne senden wir dir die Kontaktinformationen von ${user_2.first_name} ${user_2.last_name[0]}. :\n`
+        body += `${user_2.phone_number == "+41" ? "" : `Telefonnummer: ${user_2.phone_number}\n`}E-Mail-Adresse: ${user_2.email}\nVor- und Nachname: ${user_2.first_name} ${user_2.last_name}\n
         `
         return {
             body, to: user_1.phone_number
@@ -19,13 +19,13 @@ module.exports = {
     "NEW_CHAT_MESSAGE": ({ user_1, user_2, task }) => {
         let isAnswer = task.UserId === user_2.id;
         if (isAnswer) {
-            let body = `${user_2.first_name} ${user_2.last_name[0]} answered a question in “${task.title}” QA\nTask: ${process.env.BASE_URL}/task/${task.id}`
+            let body = `${user_2.first_name} ${user_2.last_name[0]} hat eine Frage in “${task.title}” beantwortet QA\nTask: ${process.env.BASE_URL}/task/${task.id}`
             return {
                 body,
                 to: user_1.phone_number,
             }
         } else {
-            let body = `${user_2.first_name} ${user_2.last_name[0]} asked a question in “${task.title}” QA\nTask: ${process.env.BASE_URL}/task/${task.id}`
+            let body = `${user_2.first_name} ${user_2.last_name[0]} hat eine Frage in “${task.title}” gestellt QA\nTask: ${process.env.BASE_URL}/task/${task.id}`
             return {
                 body,
                 to: user_1.phone_number,
