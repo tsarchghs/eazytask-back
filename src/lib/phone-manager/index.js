1129dcd1cb5f4a02767c27a3eeca41fcd88b67fb
gjergj <gjergjk71@gmail.com>
2021-03-26 00:41:58 +0100

    Optional phone-manager lib

--- src/lib/phone-manager/index.js
+++ src/lib/phone-manager/index.js
@@ -1,28 +1,40 @@
-const client = require('twilio')(
-    process.env.TWILIO_ACCOUNT_SID, 
-    process.env.TWILIO_AUTH_TOKEN
-);
-
-const getPhoneVerificationCodeSMS = ({ code, to }) => {
-    return {
-        from: process.env.TWILIO_NUMBER, to,
-        body: "Verification code: " + code
+if (
+    !process.env.TWILIO_ACCOUNT_SID || 
+    !process.env.TWILIO_AUTH_TOKEN
+) {
+    console.log("TWILIO credentials missing, SMS feature is disabled")
+    module.exports = {
+        getPhoneVerificationCodeSMS: () => {},
+        sendSMS: () => {},
+        sendPhoneVerificationCodeSMS: () => {}
     }
-}
-
-const sendSMS = async ({ body, from, to }) => {
-    return await client.messages.create({
-        body, from: from || process.env.TWILIO_NUMBER, to
-    })
-}
-
-const sendPhoneVerificationCodeSMS =  async ({ code, to }) => {
-    let args = getPhoneVerificationCodeSMS({ code, to })
-    await sendSMS(args);
-}
-
-module.exports = {
-    getPhoneVerificationCodeSMS,
-    sendPhoneVerificationCodeSMS,
-    sendSMS
+} else {
+    const client = require('twilio')(
+        process.env.TWILIO_ACCOUNT_SID, 
+        process.env.TWILIO_AUTH_TOKEN
+    );
+    
+    const getPhoneVerificationCodeSMS = ({ code, to }) => {
+        return {
+            from: process.env.TWILIO_NUMBER, to,
+            body: "Verification code: " + code
+        }
+    }
+    
+    const sendSMS = async ({ body, from, to }) => {
+        return await client.messages.create({
+            body, from: from || process.env.TWILIO_NUMBER, to
+        })
+    }
+    
+    const sendPhoneVerificationCodeSMS =  async ({ code, to }) => {
+        let args = getPhoneVerificationCodeSMS({ code, to })
+        await sendSMS(args);
+    }
+    
+    module.exports = {
+        getPhoneVerificationCodeSMS,
+        sendPhoneVerificationCodeSMS,
+        sendSMS
+    }
 }
